ba944249a85c153f13d6aabaffe52943ac9be184
nguyenhh <nhhai1605@gmail.com>
2024-12-12 21:04:02 +0700

    Add init + error handling

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,4 +9,15 @@ namespace weirditor;
 /// </summary>
 public partial class App : Application
 {
+    private void App_OnStartup(object sender, StartupEventArgs e)
+    {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
+    
+    private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+        e.Handled = true;
+    }
 }
