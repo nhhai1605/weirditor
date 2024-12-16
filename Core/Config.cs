d8ccb786f1da9a98bbcad14e6bcd7a9254694c8c
nguyenhh <nhhai1605@gmail.com>
2024-12-17 01:33:49 +0700

    add tool tip and fix parent folder name in mainWindow

--- Core/Config.cs
+++ Core/Config.cs
@@ -8,7 +8,6 @@ public class Config
     public static string NewFileText = "Untitled";
     public static FontFamily DefaultFontFamily = new FontFamily("Courier New");
     public static int DefaultFontSize = 18;
-    public static int DefaultFooterFontSize = 12;
     public static bool DefaultWrap = false;
     public static FontStyle DefaultFontStyle = FontStyles.Normal;
     public static FontWeight DefaultFontWeight = FontWeights.Normal;
