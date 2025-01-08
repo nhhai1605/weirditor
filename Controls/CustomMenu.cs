2351b1fee7c0f6fc31e6ed3f61ec6aed289dc36a
nguyenhh <nhhai1605@gmail.com>
2025-01-09 01:42:43 +0700

    Change custom menu to user control

---
+++ Controls/CustomMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace weirditor.Controls;
+
+public class CustomMenu : Menu
+{
+    public static readonly DependencyProperty PopupPlacementProperty =
+        DependencyProperty.Register(
+            nameof(PopupPlacement),
+            typeof(PlacementMode),
+            typeof(CustomMenu),
+            new PropertyMetadata(PlacementMode.Bottom)); // Default placement
+
+    public PlacementMode PopupPlacement
+    {
+        get => (PlacementMode)GetValue(PopupPlacementProperty);
+        set => SetValue(PopupPlacementProperty, value);
+    }
+}
